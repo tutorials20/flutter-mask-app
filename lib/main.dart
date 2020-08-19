fd792c2046b7df7292ca3a27280d293ec3538806
Sungshik Kim <kimss@rsupport.com>
2020-08-20 06:19:51 +0900

    buildRemainingComponent

--- lib/main.dart
+++ lib/main.dart
@@ -95,8 +95,49 @@ class _MyHomePageState extends State<MyHomePage> {
             .map((e) => ListTile(
                   title: Text(e.name),
                   subtitle: Text(e.addr),
-                  trailing: Text(e.remainStat ?? '모름'),
+                  trailing: buildRemainingComponent(e),
                 ))
             .toList(),
       );
+
+  buildRemainingComponent(Store store) {
+    var title = '판매중지';
+    var description = '';
+    var color = Colors.grey;
+    switch (store.remainStat) {
+      case 'plenty':
+        title = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        title = '보통';
+        description = '30-100';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        title = '부족';
+        description = '2-30';
+        color = Colors.red;
+        break;
+      case 'empty':
+        title = '바닥';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color, fontWeight: FontWeight.normal),
+        ),
+      ],
+    );
+  }
 }
